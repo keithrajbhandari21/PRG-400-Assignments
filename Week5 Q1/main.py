0642f6510fdd9f90835897a8ee44b84885d1e9e6
keithrajbhandari21 <keithrb.work@gmail.com>
2025-12-02 00:02:06 +0545

    Add files via upload

---
+++ Week5 Q1/main.py
@@ -0,0 +1,11 @@
+# main.py
+
+from reverse_string import reverse_string
+
+# Before value
+original = "hello"
+print("Before:", original)
+
+# After reversing
+reversed_str = reverse_string(original)
+print("After:", reversed_str)
